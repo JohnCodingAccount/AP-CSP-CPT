cc06ca648aa7f55b4291426cf6cbde1fe46e11f4
John <hcps-steelejc@henricostudents.org>
2026-03-12 21:25:20 -0400

    the grand consolidation

--- main.py
+++ main.py
@@ -1,12 +1,250 @@
 import pygame
 import sys 
 import random 
-from monster import *
-from settings import *
-from player import *
-from projectiles import *
+import math
 
-from building import *
+class Projectile():
+    def __init__(self, x, y, target_x, target_y, speed=8, damage=10, size=5, color=(250,0,0)):
+        dx = target_x - x
+        dy = target_y - y
+        dist = math.hypot(dx, dy)
+        try:
+            self.vx = dx / dist * speed
+            self.vy = dy / dist * speed
+        except ZeroDivisionError:
+            self.vy = 8
+            self.vx = 8
+        self.x = x
+        self.y = y
+        self.radius = size 
+        self.damage = damage
+        self.alive = True
+        self.color = color 
+
+    def update(self):
+        self.x += self.vx
+        self.y += self.vy
+        if self.x < 0 or self.x > 1280 or self.y < 0 or self.y > 720:
+            self.alive = False
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, (255, 80, 80), (int(self.x), int(self.y)), self.radius)
+
+    def get_damage(self):
+        return self.damage
+
+class Player():
+    def __init__(self, posx, posy, atk, hp, defs, move, col, rad, cool, sped):
+        self.position = (posx, posy)
+        self.attack_power = atk 
+        self.health = hp 
+        self.defense = defs
+        self.speed = move 
+        self.color = col 
+        self.radius = rad
+        self.cooldown = cool
+        self.projectile_speed = sped
+
+    def getPos(self):
+        return self.position
+    
+    def getX(self):
+        return self.position[0]
+
+    def getY(self):
+        return self.position[1]
+    
+    def getAtk(self):
+        return self.attack_power
+    
+    def getHP(self):
+        return self.health
+
+    def getDef(self):
+        return self.defense
+    
+    def getSpeed(self):
+        return self.speed
+    
+    def setPos(self, posx, posy):
+        self.position = (posx, posy)
+    
+    def setX(self, posx):
+        self.position = (posx, self.getY())
+
+    def setY(self, posy):
+        self.position = (self.getX(), posy)
+
+    def setAtk(self, atk):
+        self.attack_power = atk 
+    
+    def setHP(self, hp):
+        self.health = hp
+    
+    def setDef(self, defs):
+        self.defense = defs 
+
+    def setSpeed(self, speed):
+        self.speed = speed 
+
+    def move(self, px, py):
+        while True:
+            sx = self.position[0]
+            sy = self.position[1]
+            sped = self.speed
+            if py > sy:
+                sy = sped
+            elif py < sy: 
+                sy = -sped
+            else:
+                sy = 0
+            if px > sx:
+                sx = sped
+            elif px < sx:
+                sx = -sped
+            else:
+                sx = 0
+            self.position = (self.position[0] + sx , self.position[1] + sy)
+            break 
+
+    def attack(self):
+        pass 
+
+    def draw(self):
+        pass
+
+    def takeDamage(self):
+        pass 
+
+    def detectDamage(self):
+        pass 
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720 
+FPS = 60
+TITLE = "Create Performance Task"
+WAVE = 0
+MONSTERS = {}
+
+MAX_PROJECTILES = 1000
+
+class Monster:
+    def __init__(self, posx, posy, speed, atk, col, rad, health=10):
+        self.position = (posx,posy)
+        self.speed = speed 
+        self.kbSpeed = speed * 2
+        self.atk = atk 
+        self.color = col
+        self.health = health
+        self.radius = rad
+        self.alive = True
+        self.isKnocked = False
+        self.isKnocked2 = False 
+        self.kt = None 
+    def move(self, px, py):
+        dx = px - self.position[0]
+        dy = py - self.position[1]
+        dist = math.hypot(dx, dy)
+        vx = dx / dist * self.speed
+        vy = dy / dist * self.speed 
+        self.position = (self.position[0] + vx, self.position[1] + vy)
+    
+    def knockback(self, px, py):
+        self.isKnocked = True
+        dx = px - self.position[0]
+        dy = py - self.position[1]
+        dist = math.hypot(dx, dy)
+        vx = dx / dist * self.kbSpeed
+        vy = dy / dist * self.kbSpeed 
+        self.position = (self.position[0] - vx, self.position[1] - vy)
+        
+    def isAtPlayer(self, mp, pp, mr, pr):
+        mv = pygame.math.Vector2(mp)
+        pv = pygame.math.Vector2(pp)
+        d = mv.distance_to(pv)
+        if d < (mr + pr):
+            return True
+        else:
+            return False
+        
+class Spider(Monster):
+    def __init__(self, posx, posy):
+        super().__init__(posx, posy, 2, 1, (25, 25, 25), 7, 4) 
+    def move(self, px, py):
+        super().move(px, py)
+    
+    def knockback(self, px, py):
+        return super().knockback(px, py)
+    
+    def isAtPlayer(self, mp, pp, mr, pr):
+        return super().isAtPlayer(mp, pp, mr, pr)
+
+
+class Zombie(Monster):
+    def __init__(self, posx, posy):
+        super().__init__(posx, posy, 0.5, 2, (100, 100, 100), 12, 6) 
+    def move(self, px, py):
+        super().move(px, py)
+    
+    def isAtPlayer(self, mp, pp, mr, pr):
+        return super().isAtPlayer(mp, pp, mr, pr)
+    
+    def knockback(self, px, py):
+        return super().knockback(px, py)
+    
+
+
+class Boss(Monster):
+    def __init__(self, posx, posy):
+        super().__init__(posx, posy, 1.5, 5, (0, 0, 0), 50, 51) 
+    def move(self, px, py):
+        super().move(px, py)
+    
+    def knockback(self, px, py):
+        return super().knockback(px, py)
+    
+    def isAtPlayer(self, mp, pp, mr, pr):
+        return super().isAtPlayer(mp, pp, mr, pr)
+
+class Building:
+    def __init__(self, pos, health, type):
+        self.position = (pos[0], pos[1])
+        self.alive = True 
+        self.health = health
+        self.cooldown = False
+        self.st = 0 
+        self.type = type
+
+    
+    def getClosest(self, mons):
+        hd = 0
+        hmonx = 0
+        hmony = 0
+        for mon in mons: 
+            dx = mons[mon].position[0] - self.position[0]
+            dy = mons[mon].position[1] - self.position[1]
+            dist = math.hypot(dx, dy)
+            if abs(dist) > hd:
+                hd = dist
+                hmonx = mons[mon].position[0]
+                hmony = mons[mon].position[1]
+        return hmonx, hmony
+        
+class Tower(Building):
+    def __init__(self, pos, health, type):
+        super().__init__(pos, health, type)
+    
+    def getClosest(self, mons):
+        return super().getClosest(mons)
+
+class Tent(Building):
+    def __init__(self, pos, health, type):
+        super().__init__(pos, health, type)
+    
+    def getClosest(self, mons):
+        return super().getClosest(mons)
+    
+    def Heal(self):
+        pass
 
 
 grey = (51, 51, 51)
@@ -24,12 +262,12 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.font = pygame.font.Font("freesansbold.ttf", 32)
-        self.hp = 10000
-        self.cost = 1
+        self.hp = 100
+        self.cost = 10
         self.ts = self.font.render("Health: " + str(self.hp), True, (0,0,0))
         self.tr = self.ts.get_rect()
         self.tr.center = (100, 50)
-        self.gold = 100
+        self.gold = 1
         self.tg = self.font.render("Points: " + str(self.gold), True, (0,0,0))
         self.tgr = self.tg.get_rect()
         self.tgr.center = (100, 100)
@@ -51,7 +289,7 @@ class Game:
         self.towertemplate.set_alpha(100)
         pygame.mouse.set_cursor(pygame.cursors.diamond)
         pygame.display.set_caption(TITLE)
-        self.player = Player(self.screen.get_width() / 2, self.screen.get_height() / 2, 1, 10, 1, 10, "red", 10, 0.1, 8) 
+        self.player = Player(self.screen.get_width() / 2, self.screen.get_height() / 2, 1, 10, 1, 10, "red", 10, 1, 8) 
 
     
     def get_point_on_circle(self, h, k, radius):
